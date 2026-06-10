234b8add8acc71c8c3cad93b049627b0ec729c29
saranyajayapal11testleaf <saranyajayapal11@gmail.com>
2026-06-10 10:30:42 +0530

    Day1 - Primitive data types and scoping

---
+++ scoping.js
@@ -0,0 +1,15 @@
+const browserVersion = "Chrome"
+
+function getBrowserVersion () {
+    var browserVersion = "Chrome"
+    if (browserVersion = "Chrome") {
+
+        console.log("print browserversion chrome inside function",browserVersion);
+   
+    }
+    console.log("print browserversion chrome outside block",browserVersion);
+}
+console.log("print browserversion chrome outside function block",browserVersion);
+
+getBrowserVersion()
+
