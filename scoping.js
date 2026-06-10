2c19fd399f3b8be2eeda64d2cedd6e7ecf518acd
saranyajayapal11testleaf <saranyajayapal11@gmail.com>
2026-06-10 11:23:17 +0530

    Update scoping.js

--- scoping.js
+++ scoping.js
@@ -1,15 +1,12 @@
-const browserVersion = "Chrome"
+const browserVersion = "Chrome";
 
 function getBrowserVersion () {
-    var browserVersion = "Chrome"
-    if (browserVersion = "Chrome") {
-
-        console.log("print browserversion chrome inside function",browserVersion);
-   
+    if (browserVersion === "Chrome") {
+        let browserVersion = "Chrome variable";
+        console.log("print browserversion chrome inside function"+ browserVersion);
     }
-    console.log("print browserversion chrome outside block",browserVersion);
+    console.log("print browserversion chrome outside block"+ browserVersion);
 }
-console.log("print browserversion chrome outside function block",browserVersion);
 
 getBrowserVersion()
 
